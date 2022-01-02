cf3680e02eb665b92d7571ffb0450489e66d58f3
z4yx <z4yx@users.noreply.github.com>
2022-01-02 09:59:55 +0800

    tolerate malformed CHUID and CCC

--- ykman/piv.py
+++ ykman/piv.py
@@ -453,6 +453,8 @@ def get_piv_info(session: PivSession) -> str:
 
     try:
         chuid = session.get_object(OBJECT_ID.CHUID).hex()
+    except Exception:
+        chuid = "No data available."
     except ApduError as e:
         if e.sw == SW.FILE_NOT_FOUND:
             chuid = "No data available."
@@ -460,6 +462,8 @@ def get_piv_info(session: PivSession) -> str:
 
     try:
         ccc = session.get_object(OBJECT_ID.CAPABILITY).hex()
+    except Exception:
+        ccc = "No data available."
     except ApduError as e:
         if e.sw == SW.FILE_NOT_FOUND:
             ccc = "No data available."
